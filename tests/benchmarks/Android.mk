9b794acde78b24be322e8c40facb72636d58fd53
Michal Karpinski <mkarpinski@google.com>
2016-12-02 18:01:00 +0000

    DO NOT MERGE Comment out tests/benchmarks/Android.mk on nyc-mr2-dev
    netd benchmark can't build on nyc-mr2-dev, because google-benchmark project is out of date
    and won't be backported, and thus the content of this file is commented out to disable it.
    In order to run netd benchmark locally you can uncomment the content of this file and follow
    instructions in ag/1673408 (checkout that commit and build external/google-benchmark and
    system/netd locally and then run the benchmark locally)
    
    Bug: 29748723
    Change-Id: Iaa101f79f1b6b20fa856b7bf8ca95c55abe05f05

--- tests/benchmarks/Android.mk
+++ tests/benchmarks/Android.mk
@@ -13,34 +13,42 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-LOCAL_PATH := $(call my-dir)
+#
+# Note: netd benchmark can't build on nyc-mr2-dev, because google-benchmark project is out of date
+#       and won't be backported, and thus the content of this file is commented out to disable it.
+#       In order to run netd benchmark locally you can uncomment the content of this file and follow
+#       instructions in ag/1673408 (checkout that commit and build external/google-benchmark and
+#       system/netd locally and then run the benchmark locally)
+#
+#
+#LOCAL_PATH := $(call my-dir)
+#
+## APCT build target for metrics tests
+#include $(CLEAR_VARS)
+#LOCAL_MODULE := netd_benchmark
+#LOCAL_CFLAGS := -Wall -Werror -Wunused-parameter
+## Bug: http://b/29823425 Disable -Wvarargs for Clang update to r271374
+#LOCAL_CFLAGS += -Wno-varargs
 
-# APCT build target for metrics tests
-include $(CLEAR_VARS)
-LOCAL_MODULE := netd_benchmark
-LOCAL_CFLAGS := -Wall -Werror -Wunused-parameter
-# Bug: http://b/29823425 Disable -Wvarargs for Clang update to r271374
-LOCAL_CFLAGS += -Wno-varargs
+#EXTRA_LDLIBS := -lpthread
+#LOCAL_SHARED_LIBRARIES += libbase libbinder liblog libnetd_client
+#LOCAL_STATIC_LIBRARIES += libnetd_test_dnsresponder libtestUtil libutils
 
-EXTRA_LDLIBS := -lpthread
-LOCAL_SHARED_LIBRARIES += libbase libbinder liblog libnetd_client
-LOCAL_STATIC_LIBRARIES += libnetd_test_dnsresponder libtestUtil libutils
+#LOCAL_AIDL_INCLUDES := system/netd/server/binder
+#LOCAL_C_INCLUDES += system/netd/include \
+#                    system/extras/tests/include \
+#                    system/netd/client \
+#                    system/netd/server \
+#                    system/netd/server/binder \
+#                    system/netd/tests/dns_responder \
+#                    system/extras/tests/include \
+#                    bionic/libc/dns/include
 
-LOCAL_AIDL_INCLUDES := system/netd/server/binder
-LOCAL_C_INCLUDES += system/netd/include \
-                    system/extras/tests/include \
-                    system/netd/client \
-                    system/netd/server \
-                    system/netd/server/binder \
-                    system/netd/tests/dns_responder \
-                    system/extras/tests/include \
-                    bionic/libc/dns/include
+#LOCAL_SRC_FILES := main.cpp \
+#                   connect_benchmark.cpp \
+#                   dns_benchmark.cpp \
+#                   ../../server/binder/android/net/metrics/INetdEventListener.aidl
 
-LOCAL_SRC_FILES := main.cpp \
-                   connect_benchmark.cpp \
-                   dns_benchmark.cpp \
-                   ../../server/binder/android/net/metrics/INetdEventListener.aidl
+#LOCAL_MODULE_TAGS := eng tests
 
-LOCAL_MODULE_TAGS := eng tests
-
-include $(BUILD_NATIVE_BENCHMARK)
+#include $(BUILD_NATIVE_BENCHMARK)
