b905757231225cd2d37cd10aeee1bb16e4a544d2
Chenbo Feng <fengc@google.com>
2018-04-12 19:05:19 -0700

    Merge all bpf programs into one
    Multiple bpf program can be stored the same binary and we can extract
    them one by one according to their function name elf header at run time.
    Store them in one file can reduce the load time by only open one binary
    file once. Rewrite the elf parser with Slice support.
    
    Test: phone boot and eBPF program is loaded and pinned.
    Bug: 78132446
    Change-Id: I96dba91a69654fcac2c022100e954d8b0c4e0718
    Merged-In: I96dba91a69654fcac2c022100e954d8b0c4e0718
    (cherry picked from aosp commit dbbc000ae4e450b4b4ac9d545d0d1c3995949f89)

--- bpfloader/bpf_kern.c
+++ bpfloader/bpf_kern.c
@@ -17,9 +17,27 @@
 #include <linux/bpf.h>
 #include "bpf_kern.h"
 
-ELF_SEC(BPF_PROG_SEC_NAME)
+
+ELF_SEC(BPF_CGROUP_INGRESS_PROG_NAME)
+int bpf_cgroup_ingress(struct __sk_buff* skb) {
+    return bpf_traffic_account(skb, BPF_INGRESS);
+}
+
+ELF_SEC(BPF_CGROUP_EGRESS_PROG_NAME)
+int bpf_cgroup_egress(struct __sk_buff* skb) {
+    return bpf_traffic_account(skb, BPF_EGRESS);
+}
+
+ELF_SEC(XT_BPF_EGRESS_PROG_NAME)
 int xt_bpf_egress_prog(struct __sk_buff* skb) {
     uint32_t key = skb->ifindex;
     bpf_update_stats(skb, IFACE_STATS_MAP, BPF_EGRESS, &key);
+    return BPF_PASS;
+}
+
+ELF_SEC(XT_BPF_INGRESS_PROG_NAME)
+int xt_bpf_ingress_prog(struct __sk_buff* skb) {
+    uint32_t key = skb->ifindex;
+    bpf_update_stats(skb, IFACE_STATS_MAP, BPF_INGRESS, &key);
     return BPF_PASS;
 }
