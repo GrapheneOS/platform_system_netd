b70da76da38f1041cd3993fb745893e91fb755da
Maciej Żenczykowski <maze@google.com>
2019-01-28 15:20:48 -0800

    system/netd/server - add ClatUtils.{cpp,h} skeleton
    Test: builds
    Bug: 65674744
    Signed-off-by: Maciej Żenczykowski <maze@google.com>
    Change-Id: I6a3a19d78fbacc3d260eeef374c2c7255d1b83f1

---
+++ server/ClatUtilsTest.cpp
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * ClatUtilsTest.cpp - unit tests for ClatUtils.cpp
+ */
+
+#include <gtest/gtest.h>
+
+#include "ClatUtils.h"
+
+namespace android {
+namespace net {
+
+class ClatUtilsTest : public ::testing::Test {
+  public:
+    void SetUp() {}
+};
+
+}  // namespace net
+}  // namespace android
