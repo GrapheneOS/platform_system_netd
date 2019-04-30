bc8a80f07f468f73ba97253c026696d0b97f97f3
Luke Huang <huangluke@google.com>
2019-04-29 18:08:29 -0700

    Add template listener for getting OEM netd
    It is a template OemNetdListener that OEM could extend their
    binder interface based on it.
    
    Bug: 120251561
    Test: built, flashed, booted
            system/netd/tests/runtests.sh pass
    
    Merged-In: I2933eb09e5833a15507eecd898fb8d194098baa9
    Merged-In: Ib1a0fd7c0868bb1a339c79d431f62f1385f36592
    (cherry picked from commit 4315131c546585925fb4b56962791112f7f8079c)
    
    Change-Id: I2eb208724c2fb69dd0466c161ca2630524fd31c8

---
+++ server/OemNetdListener.h
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2019, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef NETD_SERVER_OEM_NETD_LISTENER_H
+#define NETD_SERVER_OEM_NETD_LISTENER_H
+
+#include "com/android/internal/net/BnOemNetd.h"
+
+namespace com {
+namespace android {
+namespace internal {
+namespace net {
+
+class OemNetdListener : public BnOemNetd {
+  public:
+    OemNetdListener() = default;
+    ~OemNetdListener() = default;
+    static ::android::sp<::android::IBinder> getListener();
+
+    ::android::binder::Status isAlive(bool* alive) override;
+
+  private:
+    std::mutex mMutex;
+    ::android::sp<::android::IBinder> mIBinder GUARDED_BY(mMutex);
+
+    ::android::sp<::android::IBinder> getIBinder() EXCLUDES(mMutex);
+};
+
+}  // namespace net
+}  // namespace internal
+}  // namespace android
+}  // namespace com
+
+#endif  // NETD_SERVER_OEM_NETD_LISTENER_H
